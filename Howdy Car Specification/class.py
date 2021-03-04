3aeb51905a00750c6fabdc830706840f4265402c
AshaR-246 <62698273+AshaR-246@users.noreply.github.com>
2021-03-04 17:48:47 +0530

    Add files via upload

---
+++ Howdy Car Specification/class.py
@@ -0,0 +1,41 @@
+class Car(Object):
+"""
+blueprint for Car
+"""
+def_init_(self, model, color, company, speed_limit):
+    self.colour=colour
+    self.model=model
+    self.company=company
+    self.speed_limit=speed_limit
+
+    def start(self):
+        print("started")
+
+    def stop(self):
+        print("stoped")
+        def accelarate(self):
+        print("accelarating....")
+
+         "accelarator functionality here"
+
+  def change_gear(self, gear_type):
+    print("gear changed")
+    " gear related functionality here"
+
+
+audi = Car("A6", "red", "audi", 80)
+
+print(audi.start())
+
+
+
+
+
+
+
+
+
+
+
+
+
